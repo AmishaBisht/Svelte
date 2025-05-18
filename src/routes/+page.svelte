c10cde0206bc96a15e0b828893fad07f9a64d995
AmishaBisht <amishabisht33@gmail.com>
2025-05-18 12:12:21 +0530

    button counter

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,18 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
+<script>
+	import Component from '$lib/Component.svelte';
+	let count = $state(0);
+
+	function increase_counter() {
+		count += 1;
+	}
+</script>
+
+<Component name="Hello from Svelte" />
+
+<button onclick={increase_counter}>
+	{#if count < 2}
+		<p>Clicked {count} time</p>
+	{:else}
+		<p>Clicked {count} times</p>
+	{/if}
+</button>
