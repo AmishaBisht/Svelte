0ac7b780c211a2c607f837a412c2eb280cb82f55
AmishaBisht <amishabisht33@gmail.com>
2025-06-11 23:07:14 +0530

    feat: add the flashcard logic

---
+++ mini_projects/Flashcard_Quiz_App/src/lib/startView.svelte
@@ -0,0 +1,16 @@
+<script lang="ts">
+	export let onStart: () => void;
+
+	function switch_view() {
+		onStart();
+	}
+</script>
+
+
+<h3>Welcome to the Flashcard Game</h3>
+<h4>
+	Test your knowledge with these flashcards. Flip the card to see the answer. Try to get them all
+	right!
+</h4>
+
+<button on:click={switch_view}>Start Quiz</button>
--- mini_projects/Flashcard_Quiz_App/src/routes/+page.svelte
+++ mini_projects/Flashcard_Quiz_App/src/routes/+page.svelte
@@ -1,2 +1,16 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
+<script lang="ts">
+	import StartView from '$lib/startView.svelte';
+	import FlashcardView from '$lib/flashcardView.svelte'; // placeholder, you'll build this
+
+	let currentView = 'start';
+
+	function goToQuiz() {
+		currentView = 'quiz';
+	}
+</script>
+
+{#if currentView === 'start'}
+	<StartView onStart={goToQuiz} />
+{:else if currentView === 'quiz'}
+	<FlashcardView />
+{/if}
